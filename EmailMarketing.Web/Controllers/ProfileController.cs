d0c1b8199714834bdbaf58860b27c8495bf877ba
Yeasin <yeasinarafat10@gmail.com>
2020-07-14 15:32:45 +0600

    Added Code for 'Change Password' of Member

---
+++ EmailMarketing.Web/Controllers/ProfileController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailMarketing.Membership.Services;
+using EmailMarketing.Web.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EmailMarketing.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationUserManager _userManager;
+        private readonly ApplicationSignInManager _signInManager;
+        private readonly ILogger<ProflileController> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public ProfileController(ApplicationSignInManager signInManager,
+           ILogger<AccountController> logger,
+           ApplicationUserManager userManager,
+           IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordModel model)
+
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if(user==null)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                    
+            }
+            return View(model);
+        }
+    }
+}
