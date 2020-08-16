b30779955d0fc99f19eda0c886ade26d2853d8c6
sa-mamun <samamun009@gmail.com>
2020-08-17 01:51:46 +0600

    code added

---
+++ EmailMarketing.Framework/Repositories/Campaings/EmailTemplateRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmailMarketing.Data;
+using EmailMarketing.Framework.Context;
+using EmailMarketing.Framework.Entities.Campaigns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailMarketing.Framework.Repositories.Campaings
+{
+    public class EmailTemplateRepository : Repository<EmailTemplate, int, FrameworkContext>, IEmailTemplateRepository
+    {
+        public EmailTemplateRepository(FrameworkContext context) : base(context) { }
+    }
+}
