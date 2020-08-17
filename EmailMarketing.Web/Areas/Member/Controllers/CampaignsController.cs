84f05623cd18ec144fdafd976fdcb7f47ff4a8ab
noyonhossen <noyonhossen1996@gmail.com>
2020-08-17 13:05:42 +0600

    code move controller to model

--- EmailMarketing.Web/Areas/Member/Controllers/CampaignsController.cs
+++ EmailMarketing.Web/Areas/Member/Controllers/CampaignsController.cs
@@ -28,62 +28,14 @@ namespace EmailMarketing.Web.Areas.Member.Controllers
             var model = new CampaignsModel();
             return View(model);
         }
-        //public async Task<IActionResult> GetCampaignReport()
-        //{
-        //    var model = new CampaignsModel();
-        //    model.CampaignReportList = await model.GetAllGroupForSelectAsync();
-        //    return View("hello");
-        //}
         public async Task<IActionResult> Export()
         {
             var model = new CampaignsModel();
-            var campaignReport = await model.GetAllGroupForSelectAsync();
-            using (var workbook = new XLWorkbook())
-            {
-                var worksheet = workbook.Worksheets.Add("Users");
-                var currentRow = 1;
-
-                worksheet.Cell(currentRow, 1).Value = "Email";
-                worksheet.Cell(currentRow, 1).Style.Font.Bold = true;
-                worksheet.Column(1).AdjustToContents();
-                worksheet.Cell(currentRow, 2).Value = "Delivered Status";
-                worksheet.Cell(currentRow, 2).Style.Font.Bold = true;
-                worksheet.Column(2).AdjustToContents();
-                worksheet.Cell(currentRow, 3).Value = "Seen Status";
-                worksheet.Cell(currentRow, 3).Style.Font.Bold = true;
-                worksheet.Column(3).AdjustToContents();
-                worksheet.Cell(currentRow, 4).Value = "Send Date";
-                worksheet.Cell(currentRow, 4).Style.Font.Bold = true;
-                worksheet.Column(4).AdjustToContents();
-                worksheet.Cell(currentRow, 5).Value = "Seen Date";
-                worksheet.Cell(currentRow, 5).Style.Font.Bold = true;
-                worksheet.Column(5).AdjustToContents();
-
-                foreach (var item in campaignReport)
-                {
-                    currentRow++;
-                    worksheet.Cell(currentRow, 1).Value = item.Email;
-                    worksheet.Column(1).AdjustToContents();
-                    worksheet.Cell(currentRow, 2).Value = item.IsDelivered == true ? "Yes" : "NO";
-                    worksheet.Column(2).AdjustToContents();
-                    worksheet.Cell(currentRow, 3).Value = item.IsSeen == true ? "Yes" : "NO";
-                    worksheet.Column(3).AdjustToContents();
-                    worksheet.Cell(currentRow, 4).Value = ""+item.SendDateTime.ToString();
-                    worksheet.Column(4).AdjustToContents();
-                    worksheet.Cell(currentRow, 5).Value = item.SeenDateTime.ToString();
-                    worksheet.Column(5).AdjustToContents();
-                }
-                using (var stream = new MemoryStream())
-                {
-                    workbook.SaveAs(stream);
-                    var content = stream.ToArray();
-
-                    return File(
+            var content = await model.GetAllGroupForSelectAsync();
+            return File(
                         content,
                         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                         "users.xlsx");
-                }
-            }
         }
     }
 }
--- EmailMarketing.Web/Areas/Member/Models/Campaigns/CampaignsModel.cs
+++ EmailMarketing.Web/Areas/Member/Models/Campaigns/CampaignsModel.cs
@@ -1,8 +1,10 @@
-﻿using EmailMarketing.Common.Services;
+﻿using ClosedXML.Excel;
+using EmailMarketing.Common.Services;
 using EmailMarketing.Framework.Entities.Campaigns;
 using EmailMarketing.Framework.Services.Campaigns;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,11 +22,53 @@ namespace EmailMarketing.Web.Areas.Member.Models.Campaigns
         {
 
         }
-        public async Task<IList<CampaignValueTextModel>> GetAllGroupForSelectAsync()
+        public async Task<byte[]> GetAllGroupForSelectAsync()
         {
-            return (await _campaignService.GetAllCampaignReportAsync(_currentUserService.UserId))
+            var campaignReport = (await _campaignService.GetAllCampaignReportAsync(_currentUserService.UserId))
                                            .Select(x => new CampaignValueTextModel { Value = x.Value, CampaignName = x.CampaignName, Email = x.Email,IsDelivered = x.IsDelivered, IsSeen = x.IsSeen, SendDateTime = x.SendDateTime, SeenDateTime = x.SeenDateTime }).ToList();
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Users");
+                var currentRow = 1;
 
+                worksheet.Cell(currentRow, 1).Value = "Email";
+                worksheet.Cell(currentRow, 1).Style.Font.Bold = true;
+                worksheet.Column(1).AdjustToContents();
+                worksheet.Cell(currentRow, 2).Value = "Delivered";
+                worksheet.Cell(currentRow, 2).Style.Font.Bold = true;
+                worksheet.Column(2).AdjustToContents();
+                worksheet.Cell(currentRow, 3).Value = "Seen";
+                worksheet.Cell(currentRow, 3).Style.Font.Bold = true;
+                worksheet.Column(3).AdjustToContents();
+                worksheet.Cell(currentRow, 4).Value = "Send Date";
+                worksheet.Cell(currentRow, 4).Style.Font.Bold = true;
+                worksheet.Column(4).AdjustToContents();
+                worksheet.Cell(currentRow, 5).Value = "Seen Date";
+                worksheet.Cell(currentRow, 5).Style.Font.Bold = true;
+                worksheet.Column(5).AdjustToContents();
+
+                foreach (var item in campaignReport)
+                {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = item.Email;
+                    worksheet.Column(1).AdjustToContents();
+                    worksheet.Cell(currentRow, 2).Value = item.IsDelivered == true ? "Yes" : "NO";
+                    worksheet.Column(2).AdjustToContents();
+                    worksheet.Cell(currentRow, 3).Value = item.IsSeen == true ? "Yes" : "NO";
+                    worksheet.Column(3).AdjustToContents();
+                    worksheet.Cell(currentRow, 4).Value = "" + item.SendDateTime.ToString();
+                    worksheet.Column(4).AdjustToContents();
+                    worksheet.Cell(currentRow, 5).Value = item.SeenDateTime.ToString();
+                    worksheet.Column(5).AdjustToContents();
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return content;    
+                }
+            }
         }
     }
 }
