e58ad4c62fa48952ec304124345bcc23b348f3cc
m-alam <m.m.alam6bs@gmail.com>
2020-08-28 16:38:42 +0600

    added group and smtp test

--- EmailMarketing.Framework.Tests/GroupServiceTests.cs
+++ EmailMarketing.Framework.Tests/GroupServiceTests.cs
@@ -16,6 +16,8 @@ using System.Text;
 using System.Threading.Tasks;
 using EmailMarketing.Common.Extensions;
 using EmailMarketing.Framework.Entities.Groups;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace EmailMarketing.Framework.Tests
 {
@@ -74,54 +76,49 @@ namespace EmailMarketing.Framework.Tests
             a.ShouldBe(group);
             _groupRepositoryMock.Verify();
         }
-        //[Test]
-        //public void GetAllAsync_GroupLists_GetAllGroup()
-        //{
-        //    //Arrange
-        //    var group = new List<Group>
-        //    {
-        //        new Group { Id = 1, Name = "Friends" },
-        //        new Group {Id = 2, Name = "Colleague"},
-        //        new Group {Id = 3, Name = "Employee"},
-        //        new Group {Id = 4, Name = "Managars"},
-        //    };
+        [Test]
+        public void GetAllAsync_GroupLists_GetAllGroup()
+        {
+            //Arrange
+            int total = 4, totalFilter = 3;
+            string searchText = "", orderBy = "Name";
+            int pageIndex = 1, pageSize = 10;
 
-        //    int total = 4, totalFilter = 3;
+            var group = new List<Group>
+            {
+                new Group { Id = 1, Name = "Friends" },
+                new Group {Id = 2, Name = "Colleague"},
+                new Group {Id = 3, Name = "Employee"},
+                new Group {Id = 4, Name = "Managars"},
+            };
 
-        //    string searchText = "", orderBy = "Name";
+            var groupToMatch = new List<Group>
+            {
+                new Group { Id = 1, Name = "Friends" },
+                new Group {Id = 2, Name = "Colleague"},
+                new Group {Id = 3, Name = "Employee"},
+                new Group {Id = 4, Name = "Managars"},
+            };
 
-        //    int pageIndex = 1, pageSize = 10;
-        //    var columnsMap = new Dictionary<string, Expression<Func<Entities.Group, object>>>()
-        //    {
-        //        ["name"] = v => v.Name
-        //    };
+            _groupUnitOfWorkMock.Setup(x => x.GroupRepository).Returns(_groupRepositoryMock.Object);
 
-
-        //    var groupToMatch = new List<Group>
-        //    {
-        //        new Group { Id = 1, Name = "Friends" },
-        //        new Group {Id = 2, Name = "Colleague"},
-        //        new Group {Id = 3, Name = "Employee"},
-        //        new Group {Id = 4, Name = "Managars"},
-        //    };
-
-        //    _groupUnitOfWorkMock.Setup(x => x.GroupRepository).Returns(_groupRepositoryMock.Object);
-        //    _groupRepositoryMock.Setup(x => x.GetAsync<Group>(y => y.  )).Returns(Task.CompletedTask).Verifiable();
-        //    //_groupRepositoryMock.Setup(x => x.GetAsync<Group>(
-        //    //    x => x, x => x.Name.Contains(searchText),
-        //    //    x => x.ApplyOrdering(columnsMap, orderBy), null,
-        //    //    pageIndex, pageSize, true)).Returns(Task.(group)).Verifiable();
+            _groupRepositoryMock.Setup(x => x.GetAsync(
+                It.Is<Expression<Func<Group, Group>>>(y => y.Compile()(new Group()) is Group),
+                It.Is<Expression<Func<Group, bool>>>(y => y.Compile()(new Group() { Name = "Employee" })),
+                It.IsAny<Func<IQueryable<Group>, IOrderedQueryable<Group>>>(),
+                It.IsAny<Func<IQueryable<Group>, IIncludableQueryable<Group, object>>>(),
+                pageIndex, pageSize, true)).ReturnsAsync((group, total, totalFilter)).Verifiable();
 
 
 
-        //    //Act
-        //    //_groupService.GetAllAsync(group);
+            //Act
+            _groupService.GetAllAsync(searchText, orderBy, pageIndex, pageSize);
 
 
-        //    //Assert
-        //    _groupRepositoryMock.VerifyAll();
-        //    _groupUnitOfWorkMock.VerifyAll();
-        //}
+            //Assert
+            _groupRepositoryMock.VerifyAll();
+            _groupUnitOfWorkMock.VerifyAll();
+        }
 
         [Test]
         public void AddAsync_GroupAlreadyExists_ThrowsException()
@@ -286,164 +283,3 @@ namespace EmailMarketing.Framework.Tests
         
     }
 }
-
-﻿//using Autofac.Extras.Moq;
-//using EmailMarketing.Common.Exceptions;
-//using EmailMarketing.Framework.Entities;
-//using EmailMarketing.Framework.Repositories.Group;
-//using EmailMarketing.Framework.Services.Groups;
-//using EmailMarketing.Framework.UnitOfWork;
-//using Moq;
-//using NUnit.Framework;
-//using Shouldly;
-//using System;
-//using System.Collections.Generic;
-//using System.Diagnostics.CodeAnalysis;
-//using System.Linq;
-//using System.Linq.Expressions;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace EmailMarketing.Framework.Tests
-//{
-//    [ExcludeFromCodeCoverage]
-//    public class GroupServiceTests
-//    {
-//        private AutoMock _mock;
-//        private Mock<IGroupRepository> _groupRepositoryMock;
-//        private Mock<IGroupUnitOfWork> _groupUnitOfWorkMock;
-//        private IGroupService _groupService;
-
-
-//        [OneTimeSetUp]
-//        public void ClassSetup()
-//        {
-//            _mock = AutoMock.GetLoose();
-//        }
-
-//        [OneTimeTearDown]
-//        public void ClassCleanUp()
-//        {
-//            _mock?.Dispose();
-//        }
-
-//        [SetUp]
-//        public void Setup()
-//        {
-//            _groupUnitOfWorkMock = _mock.Mock<IGroupUnitOfWork>();
-//            _groupRepositoryMock = _mock.Mock<IGroupRepository>();
-//            _groupService = _mock.Create<GroupService>();
-//        }
-
-//        [TearDown]
-//        public void Clean()
-//        {
-//            _groupUnitOfWorkMock.Reset();
-//            _groupRepositoryMock.Reset();
-//        }
-
-//        [Test]
-//        public async Task AddAsync_GroupAlreadyExists_ThrowsException()
-//        {
-            
-//            // Arrange
-//            var group = new Group()
-//            {
-//                Id = 1,
-//                Name = "TeamA"
-//                //UserId = Guid.NewGuid(),
-//                //Created = DateTime.Now,
-//                //CreatedBy = null,
-//                //IsActive = true,
-//                //IsDeleted = false,
-//                //LastModified = DateTime.Now,
-//                //LastModifiedBy = null
-//            };
-
-//            var groupToMatch = new Group
-//            {
-//                Id = 2,
-//                Name = "TeamA"
-//            };
-
-//            _groupUnitOfWorkMock.Setup(x => x.GroupRepository)
-//                .Returns(_groupRepositoryMock.Object);
-
-//            _groupRepositoryMock.Setup(x => x.IsExistsAsync(
-//                It.Is<Expression<Func<Group, bool>>>(y => y.Compile()(groupToMatch))))
-//                .Returns(new Task<bool>(() => true)).Verifiable();
-
-//            // Act
-            
-//              Should.Throw<DuplicationException>(async() =>
-//               await _groupService.AddAsync(group)
-//           );
-
-//            // Assert
-//            _groupRepositoryMock.VerifyAll();
-//        }
-
-//        #region NotTestedYet
-//        public void GetAllAsync_Test()
-//        {
-//            // Arrange
-//            List<Group> group = new List<Group>()
-//            {
-//                new Group()
-//                {
-//                    Id = 1,
-//                    UserId = Guid.NewGuid(),
-//                    Name = "TeamA",
-//                    Created = DateTime.Now,
-//                    CreatedBy = null,
-//                    IsActive = true,
-//                    IsDeleted = false,
-//                    LastModified = DateTime.Now,
-//                    LastModifiedBy = null
-
-//                },
-//                new Group()
-//                {
-//                    Id = 1,
-//                    UserId = Guid.NewGuid(),
-//                    Name = "TeamB",
-//                    Created = DateTime.Now,
-//                    CreatedBy = null,
-//                    IsActive = true,
-//                    IsDeleted = false,
-//                    LastModified = DateTime.Now,
-//                    LastModifiedBy = null
-
-//                },
-//                new Group()
-//                {
-//                    Id = 1,
-//                    UserId = Guid.NewGuid(),
-//                    Name = "TeamB",
-//                    Created = DateTime.Now,
-//                    CreatedBy = null,
-//                    IsActive = true,
-//                    IsDeleted = false,
-//                    LastModified = DateTime.Now,
-//                    LastModifiedBy = null
-
-//                }
-
-//            };
-
-//            _groupUnitOfWorkMock.Setup(x => x.GroupRepository)
-//                .Returns(_groupRepositoryMock.Object);
-
-//            //_groupRepositoryMock.Setup(x => x.GetAsync<Group>(
-//            //    It.Is<Expression<Func<Group, bool>>>(y => y.Compile()(new List<Group>(() => group))))
-//            //    .Returns((group,2,0)).Verifiable());
-
-//            // Act
-
-
-//        }
-//        #endregion
-
-//    }
-//}
-
---
+++ EmailMarketing.Framework.Tests/SMTPServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using Autofac.Extras.Moq;
+using EmailMarketing.Framework.Entities.SMTP;
+using EmailMarketing.Framework.Repositories.SMTP;
+using EmailMarketing.Framework.Services.SMTP;
+using EmailMarketing.Framework.UnitOfWorks.SMTP;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EmailMarketing.Framework.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class SMTPServiceTests
+    {
+        private AutoMock _mock;
+        private Mock<ISMTPRepository> _smtpRepositoryMock;
+        private Mock<ISMTPUnitOfWork> _smtpUnitOfWorkMock;
+        private ISMTPService _smtpService;
+
+        [OneTimeSetUp]
+        public void ClassSetup()
+        {
+            _mock = AutoMock.GetLoose();
+        }
+
+        [OneTimeTearDown]
+        public void ClassCleanUp()
+        {
+            _mock?.Dispose();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _smtpRepositoryMock = _mock.Mock<ISMTPRepository>();
+            _smtpUnitOfWorkMock = _mock.Mock<ISMTPUnitOfWork>();
+            _smtpService = _mock.Create<SMTPService>();
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            _smtpRepositoryMock.Reset();
+            _smtpUnitOfWorkMock.Reset();
+        }
+
+        [Test]
+        public void GetByIdAsync_GroupId_ReturnsGroupObject()
+        {
+            //Arrange
+            var group = new SMTPConfig
+            {
+                 //UserId = 1,
+                 Server = "Example",
+                 Port = 8080,
+                 SenderName = "ABC",
+                 SenderEmail ="ABC@gmail.com",
+                 UserName = "Abc",
+                 Password = "12345",
+                 EnableSSL = true
+            };
+        
+        }
+
+    }
+}
