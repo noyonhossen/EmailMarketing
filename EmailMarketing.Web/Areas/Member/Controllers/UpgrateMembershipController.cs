16f5b27af01f61131fef204e449cca44e292482f
noyonhossen <noyonhossen1996@gmail.com>
2020-09-03 23:12:10 +0600

    home pages & member pages design and fix

---
+++ EmailMarketing.Web/Areas/Member/Controllers/UpgrateMembershipController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailMarketing.Web.Areas.Member.Models.Memberships;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailMarketing.Web.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class UpgrateMembershipController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new UpgrateMembershipModel();
+            return View(model);
+        }
+    }
+}
